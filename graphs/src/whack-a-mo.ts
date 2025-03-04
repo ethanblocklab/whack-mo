9032281039cbaa7953e563406dc1b7532963b1d1
ethanblocklab <x.ethan.tec@gmail.com>
2025-03-04 11:18:46 +0800

    Initialize subgraph

---
+++ graphs/src/whack-a-mo.ts
@@ -0,0 +1,17 @@
+import { MoWhacked as MoWhackedEvent } from "../generated/WhackAMo/WhackAMo"
+import { MoWhacked } from "../generated/schema"
+
+export function handleMoWhacked(event: MoWhackedEvent): void {
+  let entity = new MoWhacked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.player = event.params.player
+  entity.points = event.params.points
+  entity.newTotalPoints = event.params.newTotalPoints
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ graphs/tests/whack-a-mo-utils.ts
@@ -0,0 +1,28 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import { MoWhacked } from "../generated/WhackAMo/WhackAMo"
+
+export function createMoWhackedEvent(
+  player: Address,
+  points: BigInt,
+  newTotalPoints: BigInt
+): MoWhacked {
+  let moWhackedEvent = changetype<MoWhacked>(newMockEvent())
+
+  moWhackedEvent.parameters = new Array()
+
+  moWhackedEvent.parameters.push(
+    new ethereum.EventParam("player", ethereum.Value.fromAddress(player))
+  )
+  moWhackedEvent.parameters.push(
+    new ethereum.EventParam("points", ethereum.Value.fromUnsignedBigInt(points))
+  )
+  moWhackedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newTotalPoints",
+      ethereum.Value.fromUnsignedBigInt(newTotalPoints)
+    )
+  )
+
+  return moWhackedEvent
+}
